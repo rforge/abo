ac1ae3da123f6dd53237f810e3e9db8185eff05e
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-27 01:03:43 +0000

    Version: 0.7.7 [2010-09-26] o Now postsegmentTCN() can process multiple chromosomes.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abo@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/psCBS/R/PairedPSCBS.EXTS.R
+++ pkg/psCBS/R/PairedPSCBS.EXTS.R
@@ -119,87 +119,135 @@ setMethodS3("postsegmentTCN", "PairedPSCBS", function(fit, ..., verbose=FALSE) {
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Validate arguments
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
+  # Argument 'verbose':
+  verbose <- Arguments$getVerbose(verbose);
+  if (verbose) {
+    pushState(verbose);
+    on.exit(popState(verbose));
+  }
+ 
+  verbose && enter(verbose, "Post-segmenting TCNs");
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   # Extract the data and segmentation results
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   data <- fit$data;
   stopifnot(!is.null(data));
+
+  segs <- fit$output;
+  stopifnot(!is.null(segs));
+  nbrOfSegments <- nrow(segs);
+  verbose && cat(verbose, "Number of segments: ", nbrOfSegments);
+
+  chromosome <- segs$chromosome;
+  chromosomes <- sort(unique(chromosome), na.last=TRUE);
+  nbrOfChromosomes <- length(chromosomes);
+  if (nbrOfChromosomes > 1) {
+    # Ignore NA chromosomes, because they are dividers
+    chromosomes <- chromosomes[is.finite(chromosomes)];
+  }
+  nbrOfChromosomes <- length(chromosomes);
+
+  verbose && cat(verbose, "Number of chromosomes: ", nbrOfChromosomes);
+  verbose && print(verbose, chromosomes);
+
+
+  chromosome <- data$chromosome;
   x <- data$x;
   CT <- data$CT;
   muN <- data$muN;
   isSnp <- !is.na(muN);
   isHet <- isSnp & (muN == 1/2);
-  nbrOfLoci <- length(x);
-
-  segs <- fit$output;
-  stopifnot(!is.null(segs));
-  nbrOfSegments <- nrow(segs);
 
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   # Update the TCN segments
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-  tcnIds <- sort(unique(segs[["tcn.id"]]));
-  I <- length(tcnIds);
-  for (ii in seq(length=I)) {
-    tcnId <- tcnIds[ii];
-    rows <- which(segs[["tcn.id"]] == tcnId);
-    segsII <- segs[rows,,drop=FALSE];
+  for (cc in seq(length=nbrOfChromosomes)) {
+    chr <- chromosomes[cc];
+    chrTag <- sprintf("chr%02d", chr);
+    verbose && enter(verbose, sprintf("Chromosome %d ('%s') of %d", chr, chrTag, 22));
+    rows <- which(is.element(segs[["chromosome"]], chr));
+    segsCC <- segs[rows,,drop=FALSE];
+    nbrOfSegmentsCC <- nrow(segsCC);
+    verbose && cat(verbose, "Number of segments: ", nbrOfSegmentsCC);
 
-    J <- nrow(segsII);
-    for (jj in seq(length=J)) {
-      seg <- segsII[jj,,drop=FALSE];
+    tcnIds <- sort(unique(segsCC[["tcn.id"]]));
+    I <- length(tcnIds);
+    for (ii in seq(length=I)) {
+      tcnId <- tcnIds[ii];
+      verbose && enter(verbose, sprintf("TCN segment %d ('%s') of %d", ii, tcnId, I));
 
-      # (a) Start and end of TCN segment is (somewhere) in 
-      #     the middle of the end of previous DH segment and
-      #     the start of the current DH segment.
-      if (jj == 1) {
-        start <- seg[["tcn.loc.start"]];
-      } else if (jj > 1) {
-        starts <- c(segsII[jj-1,"dh.loc.end"], seg[["dh.loc.start"]]);
-        start <- mean(starts);
-      }
-      if (jj == J) {
-        end <- seg[["tcn.loc.end"]];
-      } else if (jj < J) {
-        ends <- c(seg[["dh.loc.end"]], segsII[jj+1,"dh.loc.start"]);
-        end <- mean(ends);
-      }
+      rowsII <- which(segsCC[["tcn.id"]] == tcnId);
+      segsII <- segsCC[rowsII,,drop=FALSE];
+  
+      J <- nrow(segsII);
+      for (jj in seq(length=J)) {
+        verbose && enter(verbose, sprintf("DH segment %d of %d", jj, J));
+        seg <- segsII[jj,,drop=FALSE];
+  
+        # (a) Start and end of TCN segment is (somewhere) in 
+        #     the middle of the end of previous DH segment and
+        #     the start of the current DH segment.
+        if (jj == 1) {
+          start <- seg[["tcn.loc.start"]];
+        } else if (jj > 1) {
+          starts <- c(segsII[jj-1,"dh.loc.end"], seg[["dh.loc.start"]]);
+          start <- mean(starts);
+        }
+        if (jj == J) {
+          end <- seg[["tcn.loc.end"]];
+        } else if (jj < J) {
+          ends <- c(seg[["dh.loc.end"]], segsII[jj+1,"dh.loc.start"]);
+          end <- mean(ends);
+        }
+  
+        # (b) Find the units within this first guess of the TCN segment
+        #     HB: (x <= end) or (x < end); is it possible that we
+        #         include the same locus in two segments? /HB 2010-09-21
+        units <- (chromosome == chr & start <= x & x <= end);
+  
+        # (c) Adjust the start and end of the TCN segment
+        xJJ <- x[units];
+        start <- min(xJJ, na.rm=TRUE);
+        end <- max(xJJ, na.rm=TRUE);
+  
+        # (d) Identify the actual units
+        units <- (chromosome == chr & start <= x & x <= end);
+        units <- whichVector(units);
+        verbose && cat(verbose, "Units:");
+        verbose && str(verbose, units);
+  
+        # Update the segment, estimates and counts
+        seg[["tcn.loc.start"]] <- start;
+        seg[["tcn.loc.end"]] <- end;
+        seg[["tcn.mean"]] <- mean(CT[units], na.rm=TRUE);
+        seg[["tcn.num.mark"]] <- length(units);
+        seg[["tcn.num.snps"]] <- sum(isSnp[units]);
+        seg[["tcn.num.hets"]] <- sum(isHet[units]);
+  
+        segsII[jj,] <- seg;
+        rm(xJJ, start, end, units, seg);
 
-      # (b) Find the units within this first guess of the TCN segment
-      #     HB: (x <= end) or (x < end); is it possible that we
-      #         include the same locus in two segments? /HB 2010-09-21
-      units <- (start <= x & x <= end);
-      units <- whichVector(units);
+        verbose && exit(verbose);
+      } # for (jj ...)
 
-      # (c) Adjust the start and end of the TCN segment
-      xJJ <- x[units];
-      start <- min(xJJ, na.rm=TRUE);
-      end <- max(xJJ, na.rm=TRUE);
+      segsCC[rowsII,] <- segsII;
+      rm(rowsII, segsII);
+      verbose && exit(verbose);
+    } # for (ii ...)
 
-      # (d) Identify the actual units
-      units <- (start <= x & x <= end);
-      units <- whichVector(units);
-
-      # Update the segment, estimates and counts
-      seg[["tcn.loc.start"]] <- start;
-      seg[["tcn.loc.end"]] <- end;
-      seg[["tcn.mean"]] <- mean(CT[units], na.rm=TRUE);
-      seg[["tcn.num.mark"]] <- length(units);
-      seg[["tcn.num.snps"]] <- sum(isSnp[units]);
-      seg[["tcn.num.hets"]] <- sum(isHet[units]);
-
-      segsII[jj,] <- seg;
-    } # for (jj ...)
-    segs[rows,] <- segsII;
-  } # for (ii ...)
+    segs[rows,] <- segsCC;
+    rm(rows, segsCC);
+    verbose && exit(verbose);
+  } # for (cc ...)
 
   # Return results
   fitS <- fit;
   fitS$data <- data;
   fitS$output <- segs;
+
+  verbose && exit(verbose);
 
   fitS;
 }) # postsegmentTCN()
@@ -208,6 +256,8 @@ setMethodS3("postsegmentTCN", "PairedPSCBS", function(fit, ..., verbose=FALSE) {
 
 ############################################################################
 # HISTORY:
+# 2010-09-26
+# o Now postsegmentTCN() processes multiple chromosomes.
 # 2010-09-21
 # o Added postsegmentTCN() for PairedPSCBS.
 # 2010-09-19
