83eb126904c45128f6250e990b3ed54a3f940156
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-25 18:32:28 +0000

    Version: 0.9.3 [2010-10-25] o Added argument 'ciRange' to callAllelicBalance() and callExtremeAllelicImbalance().
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abo@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/psCBS/R/PairedPSCBS.CALL.R
+++ pkg/psCBS/R/PairedPSCBS.CALL.R
@@ -1,12 +1,27 @@
-setMethodS3("callAllelicBalance", "PairedPSCBS", function(fit, tau=0.10, ...) {
+setMethodS3("callAllelicBalance", "PairedPSCBS", function(fit, tau=0.10, ciRange=c(0.05, 0.95), ...) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
   # Argument 'tau':
   tau <- Arguments$getDouble(tau, range=c(0,Inf));
 
+  # Argument 'ciRange':
+  ciRange <- Arguments$getDoubles(ciRange, range=c(0,1), length=c(2,2));
+
+
   # Calculate DH confidence intervalls, if not already done
-  fit <- bootstrapDHByRegion(fit);
+  statsFcn <- function(x) quantile(x, probs=ciRange);
+  fit <- bootstrapDHByRegion(fit, statsFcn=statsFcn, ...);
 
   segs <- as.data.frame(fit);
-  ci <- segs[,c("dh.ci.5%", "dh.ci.95%"), drop=FALSE];
+
+  # Extract confidence interval
+  ciRangeTags <- sprintf("%g%%", 100*ciRange);
+  columns <- sprintf("dh.ci.%s", ciRangeTags);
+  stopifnot(all(is.element(columns, colnames(segs))));
+
+  ci <- segs[,columns, drop=FALSE];
+
   call <- (ci[,1] < tau);
   segs <- cbind(segs, ab.call=call);
   fit$output <- segs;
@@ -15,15 +30,30 @@ setMethodS3("callAllelicBalance", "PairedPSCBS", function(fit, tau=0.10, ...) {
 })
 
 
-setMethodS3("callExtremeAllelicImbalance", "PairedPSCBS", function(fit, tau=0.60, ...) {
+setMethodS3("callExtremeAllelicImbalance", "PairedPSCBS", function(fit, tau=0.60, ciRange=c(0.05, 0.95), ...) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Validate arguments
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  
   # Argument 'tau':
   tau <- Arguments$getDouble(tau, range=c(0,Inf));
 
+  # Argument 'ciRange':
+  ciRange <- Arguments$getDoubles(ciRange, range=c(0,1), length=c(2,2));
+
+
   # Calculate DH confidence intervalls, if not already done
-  fit <- bootstrapDHByRegion(fit);
+  statsFcn <- function(x) quantile(x, probs=ciRange);
+  fit <- bootstrapDHByRegion(fit, statsFcn=statsFcn, ...);
 
   segs <- as.data.frame(fit);
-  ci <- segs[,c("dh.ci.5%", "dh.ci.95%"), drop=FALSE];
+
+  # Extract confidence interval
+  ciRangeTags <- sprintf("%g%%", 100*ciRange);
+  columns <- sprintf("dh.ci.%s", ciRangeTags);
+  stopifnot(all(is.element(columns, colnames(segs))));
+
+  ci <- segs[,columns, drop=FALSE];
+
   call <- (ci[,1] >= tau);
   segs <- cbind(segs, ai.high.call=call);
   fit$output <- segs;
@@ -41,6 +71,11 @@ setMethodS3("callABandHighAI", "PairedPSCBS", function(fit, tauAB=0.10, tauHighA
 
 ##############################################################################
 # HISTORY
+# 2010-10-25 [HB]
+# o Now also arguments '...' to callAllelicBalance() and
+#   callExtremeAllelicImbalance() are passed to bootstrapDHByRegion().
+# o Added argument 'ciRange' to callAllelicBalance() and
+#   callExtremeAllelicImbalance().
 # 2010-09-16 [HB]
 # o Added callABandHighAI().
 # o Added callAllelicBalance() and callExtremeAllelicImbalance().
