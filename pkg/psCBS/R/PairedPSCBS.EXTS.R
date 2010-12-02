32e350d433d1e6fb218710cc9718f9928574bf93
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-12-02 17:52:43 +0000

    Version: 0.9.30 [2010-12-01] o Rewrote all code dealing with the identification of loci belong   to segments.  The code is now utilizing the 'segRows' element   returned by DNAcopy::segment().  Lots of the code was rewritten   and therefore completely new bugs may have been introduced.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abo@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/psCBS/R/PairedPSCBS.EXTS.R
+++ pkg/psCBS/R/PairedPSCBS.EXTS.R
@@ -90,6 +90,11 @@ setMethodS3("postsegmentTCN", "PairedPSCBS", function(fit, ..., force=FALSE, ver
     return(fit);
   }
 
+  joinSegments <- fit$params$joinSegments;
+  if (!joinSegments) {
+    throw("Postsegmentation of TCNs is only implemented for the case when joinSegments=TRUE: ", joinSegments);
+  }
+
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
   # Extract the data and segmentation results
@@ -155,9 +160,9 @@ setMethodS3("postsegmentTCN", "PairedPSCBS", function(fit, ..., force=FALSE, ver
 
       tcnSegRowsII <- tcnSegRowsCC[rowsII,,drop=FALSE];
       dhSegRowsII <- dhSegRowsCC[rowsII,,drop=FALSE];
-print(tcnSegRowsII);
-print(dhSegRowsII);
-
+##print(tcnSegRowsII);
+##print(dhSegRowsII);
+      tcnSegRowsIIBefore <- tcnSegRowsII;
       nbrOfTCNsBefore <- segsII[1,"tcn.num.mark"];
 
       for (jj in seq(length=J)) {
@@ -168,69 +173,103 @@ print(dhSegRowsII);
         # Sanity check
         stopifnot(all(is.na(tcnSegRow)) || (tcnSegRow[1] <= tcnSegRow[2]));
         stopifnot(all(is.na(dhSegRow)) || (dhSegRow[1] <= dhSegRow[2]));
-  
-        # (a) Start and end of TCN segment is (somewhere) in 
-        #     the middle of the end of previous DH segment and
-        #     the start of the current DH segment.
-        if (jj == 1) {
-          xStart <- seg[["tcn.loc.start"]];
-          idxStart <- tcnSegRow[1];
-        } else if (jj > 1) {
-          xStarts <- c(segsII[jj-1,"dh.loc.end"], seg[["dh.loc.start"]]);
-          xStart <- mean(xStarts);
-          idxStart <- max(dhSegRow[1], tcnSegRow[1], na.rm=TRUE);
-          idxStart <- min(idxEnd+1L, idxStart);
-        }
-        if (jj == J) {
-          xEnd <- seg[["tcn.loc.end"]];
-          idxEnd <- tcnSegRow[2];
-        } else if (jj < J) {
-          xEnds <- c(seg[["dh.loc.end"]], segsII[jj+1,"dh.loc.start"]);
-          xEnd <- mean(xEnds);
-          idxEnd <- min(dhSegRow[2], tcnSegRow[2], na.rm=TRUE);
-        }
-
-        verbose && printf(verbose, "[xStart,xEnd] = [%g,%g]\n", xStart, xEnd);
-        verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", idxStart, idxEnd);
 
         # Sanity check
-        stopifnot(xStart <= xEnd);
-        stopifnot(idxStart <= idxEnd);
-
-        # (b) Identify units
-        units <- which(chromosome == chr & xStart <= x & x <= xEnd);
-        verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", min(units), max(units));
-        verbose && cat(verbose, "Number of initial loci: ", length(units));
-
-
-        # (c) Drop if it belongs to neighboring segment
-        keep <- which(idxStart <= units & units <= idxEnd);
-        nbrOfDropped <- length(units) - length(keep);
-        verbose && cat(verbose, "Number of loci dropped: ", nbrOfDropped);
-        units <- units[keep];
-        xJJ <- x[units];
-        xSupport <- range(xJJ, na.rm=FALSE);
-        verbose && printf(verbose, "[xStart,xEnd] = [%g,%g]\n", xSupport[1], xSupport[2]);
-        verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", min(units), max(units));
-        verbose && cat(verbose, "Number of loci: ", length(units));
-
-        # (c) Adjust the start and end of the TCN segment
-        keep <- which(!is.na(xJJ));
-        xJJ <- xJJ[keep];
-        units <- units[keep];
-        tcnSegRow <- range(units);
-        xSupport <- range(xJJ, na.rm=FALSE);
-        verbose && printf(verbose, "[xStart,xEnd] = [%g,%g]\n", xSupport[1], xSupport[2]);
-        verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", tcnSegRow[1], tcnSegRow[2]);
-        verbose && cat(verbose, "Number of loci: ", length(units));
-
-        xRange <- range(c(xStart,xEnd), xSupport, na.rm=TRUE);
+        idxsTCN <- tcnSegRow[1]:tcnSegRow[2];
+        nbrOfTCNs <- sum(!is.na(CT[idxsTCN]));
+        stopifnot(nbrOfTCNs == nbrOfTCNsBefore);
   
+        if (joinSegments) {
+          # (a) The TCN segment should have identical (start,end) boundaries as the DH region
+          xStart <- seg[["dh.loc.start"]];
+          xEnd <- seg[["dh.loc.end"]];
+          verbose && printf(verbose, "[xStart,xEnd] = [%.0f,%.0f]\n", xStart, xEnd);
+          stopifnot(xStart <= xEnd);
+
+          # (b) Identify units
+          units <- which(chromosome == chr & xStart <= x & x <= xEnd);
+          tcnSegRow <- range(units);
+          verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", tcnSegRow[1], tcnSegRow[2]);
+          verbose && cat(verbose, "Number of TCN loci: ", length(units));
+
+          # (c) Adjust for missing values
+          keep <- which(!is.na(CT[units]));
+          units <- units[keep];
+          tcnSegRow <- range(units);
+          verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", tcnSegRow[1], tcnSegRow[2]);
+          verbose && cat(verbose, "Number of non-missing TCN loci: ", length(units));
+        } else {
+          throw("Not implemented correctly.")  # /HB 2010-12-02
+          # (a) Start and end of TCN segment is (somewhere) in 
+          #     the middle of the end of previous DH segment and
+          #     the start of the current DH segment.
+          if (jj == 1) {
+            xStart <- seg[["tcn.loc.start"]];
+            idxStart <- tcnSegRow[1];
+          } else if (jj > 1) {
+            xStarts <- c(segsII[jj-1,"dh.loc.end"], seg[["dh.loc.start"]]);
+            xStart <- mean(xStarts);
+            idxStart <- max(dhSegRow[1], tcnSegRow[1], na.rm=TRUE);
+            idxStart <- min(idxEnd+1L, idxStart);
+          }
+          if (jj == J) {
+            xEnd <- seg[["tcn.loc.end"]];
+            idxEnd <- tcnSegRow[2];
+          } else if (jj < J) {
+            xEnds <- c(seg[["dh.loc.end"]], segsII[jj+1,"dh.loc.start"]);
+            xEnd <- mean(xEnds);
+            idxEnd <- min(dhSegRow[2], tcnSegRow[2], na.rm=TRUE);
+          }
+   
+          verbose && printf(verbose, "[xStart,xEnd] = [%.0f,%.0f]\n", xStart, xEnd);
+          verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", idxStart, idxEnd);
+          verbose && cat(verbose, "Number of initial loci: ", idxEnd-idxStart+1L);
+  
+          # Sanity check
+          stopifnot(xStart <= xEnd);
+          stopifnot(idxStart <= idxEnd);
+  
+          # (b) Identify units
+          units <- which(chromosome == chr & xStart <= x & x <= xEnd);
+          verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", min(units), max(units));
+          verbose && cat(verbose, "Number of initial loci: ", length(units));
+  
+          # (c) Drop if it belongs to neighboring segment
+          keep <- which(idxStart <= units & units <= idxEnd);
+          nbrOfDropped <- length(units) - length(keep);
+          verbose && cat(verbose, "Number of loci dropped: ", nbrOfDropped);
+          units <- units[keep];
+          xJJ <- x[units];
+          xSupport <- range(xJJ, na.rm=FALSE);
+          verbose && printf(verbose, "[xStart,xEnd] = [%.0f,%.0f]\n", xSupport[1], xSupport[2]);
+          verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", min(units), max(units));
+          verbose && cat(verbose, "Number of loci: ", length(units));
+  
+          # (c) Adjust the start and end of the TCN segment
+          keep <- which(!is.na(xJJ));
+          xJJ <- xJJ[keep];
+          units <- units[keep];
+          tcnSegRow <- range(units);
+          xSupport <- range(xJJ, na.rm=FALSE);
+          verbose && printf(verbose, "[xStart,xEnd] = [%.0f,%.0f]\n", xSupport[1], xSupport[2]);
+          verbose && printf(verbose, "[idxStart,idxEnd] = [%d,%d]\n", tcnSegRow[1], tcnSegRow[2]);
+          verbose && cat(verbose, "Number of loci: ", length(units));
+  
+          xRange <- range(c(xStart,xEnd), xSupport, na.rm=TRUE);
+
+          xStart <- xRange[1];
+          xEnd <- xRange[2];
+        } # if (joinSegments)
+  
+        gamma <- mean(CT[units]);
+        # Sanity check
+        stopifnot(length(units) == 0 || !is.na(gamma));
+
         # Update the segment boundaries, estimates and counts
-        seg[["tcn.loc.start"]] <- xRange[1];
-        seg[["tcn.loc.end"]] <- xRange[2];
-        seg[["tcn.mean"]] <- mean(CT[units], na.rm=TRUE);
-        seg[["tcn.num.mark"]] <- sum(!is.na(CT[units]));
+        seg[["tcn.loc.start"]] <- xStart;
+        seg[["tcn.loc.end"]] <- xEnd;
+        seg[["tcn.mean"]] <- gamma;
+        seg[["tcn.num.mark"]] <- length(units);
         seg[["tcn.num.snps"]] <- sum(isSnp[units]);
         seg[["tcn.num.hets"]] <- sum(isHet[units]);
 
@@ -246,14 +285,15 @@ print(dhSegRowsII);
       # Sanity check
       stopifnot(nrow(segsII) == length(rowsII));
 
-print(segsII);
+##print(segsII);
+##print(tcnSegRowsIIBefore);
+##print(tcnSegRowsII);
 
       # Sanity check
       nbrOfTCNsAfter <- sum(segsII[,"tcn.num.mark"], na.rm=TRUE);
       verbose && cat(verbose, "Number of TCNs before: ", nbrOfTCNsBefore);
       verbose && cat(verbose, "Number of TCNs after: ", nbrOfTCNsAfter);
-#      stopifnot(nbrOfTCNsAfter >= nbrOfTCNsBefore);
-
+      stopifnot(nbrOfTCNsAfter >= nbrOfTCNsBefore);
 
 
       segsCC[rowsII,] <- segsII;
@@ -302,6 +342,10 @@ print(segsII);
 
 ############################################################################
 # HISTORY:
+# 2010-12-02
+# o Now postsegmentTCN() assert that total number of TCN loci before
+#   and after is the same.
+# o Now postsegmentTCN() assert that joinSegment is TRUE.
 # 2010-12-01
 # o Now postsegmentTCN() checks if it is already postsegmented.
 # 2010-11-30
