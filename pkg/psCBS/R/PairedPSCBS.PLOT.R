3680351353aafd875b7857e4f8e59ecd27caef43
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-01-18 20:29:00 +0000

    Version: 0.9.38 [2011-01-18] o DOCUMENTATION: Documented more plotTracks() arguments for PairedPSCBS. o BUG FIX: Now plotTracks(..., add=TRUE) for PairedPSCBS plots to   the current figure/panel. o Now plotTracks(..., add=FALSE) for PairedPSCBS only sets up subplots   if argument 'tracks' specifies more than one panel. o Added arguments 'changepoints' and 'col' to plotTracks() for PairedPSCBS.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abo@90 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/psCBS/R/PairedPSCBS.PLOT.R
+++ pkg/psCBS/R/PairedPSCBS.PLOT.R
@@ -20,7 +20,13 @@
 #     have scatter plot.}
 #   \item{calls}{A @character @vector of regular expression identifying
 #     call labels to be highlighted in the panels.}
-#   \item{pch}{The type of points to use.}
+#   \item{pch}{The type of the scatter points, if any.}
+#   \item{col}{The color of the scatter points, if any.}
+#   \item{cex}{The size of the scatter points, if any.}
+#   \item{changepoints}{If @TRUE, changepoints are drawn as vertical lines.}
+#   \item{grid}{If @TRUE, horizontal lines are displayed.}
+#   \item{quantiles}{A @numeric @vector in [0,1] specifying the quantiles
+#      of the confidence bands to be drawn, if any.}
 #   \item{xlim}{(Optional) The genomic range to plot.}
 #   \item{Clim}{The range of copy numbers.}
 #   \item{Blim}{The range of allele B fractions (BAFs) and 
@@ -41,7 +47,8 @@
 # @keyword IO
 # @keyword internal
 #*/########################################################################### 
-setMethodS3("plotTracks", "PairedPSCBS", function(x, tracks=c("tcn", "dh", "tcn,c1,c2", "tcn,c1", "tcn,c2", "c1,c2", "betaN", "betaT", "betaTN")[1:3], scatter="*", calls=".*", pch=".", quantiles=c(0.05,0.95), cex=1, grid=FALSE, xlim=NULL, Clim=c(0,6), Blim=c(0,1), xScale=1e-6, ..., add=FALSE, verbose=FALSE) {
+setMethodS3("plotTracks", "PairedPSCBS", function(x, tracks=c("tcn", "dh", "tcn,c1,c2", "tcn,c1", "tcn,c2", "c1,c2", "betaN", "betaT", "betaTN")[1:3], scatter="*", calls=".*", pch=".", col=NULL, cex=1, changepoints=FALSE, grid=FALSE, quantiles=c(0.05,0.95), xlim=NULL, Clim=c(0,6), Blim=c(0,1), xScale=1e-6, ..., add=FALSE, verbose=FALSE) {
+
   # To please R CMD check
   fit <- x;
  
@@ -76,6 +83,12 @@ setMethodS3("plotTracks", "PairedPSCBS", function(x, tracks=c("tcn", "dh", "tcn,
     calls <- sapply(calls, FUN=Arguments$getRegularExpression);
   }
 
+  # Argument 'changepoints':
+  changepoints <- Arguments$getLogical(changepoints);
+
+  # Argument 'grid':
+  grid <- Arguments$getLogical(grid);
+
   # Argument 'xlim':
   if (!is.null(xlim)) {
     xlim <- Arguments$getNumerics(xlim, length=c(2,2));
@@ -93,7 +106,6 @@ setMethodS3("plotTracks", "PairedPSCBS", function(x, tracks=c("tcn", "dh", "tcn,
     pushState(verbose);
     on.exit(popState(verbose));
   }
-
 
 
   verbose && enter(verbose, "Plotting PSCN tracks");
@@ -155,74 +167,105 @@ setMethodS3("plotTracks", "PairedPSCBS", function(x, tracks=c("tcn", "dh", "tcn,
     }
   }
 
-  if (!add) {
+  if (length(tracks) > 1 && !add) {
     subplots(length(tracks), ncol=1);
     par(mar=c(1,4,1,2)+1);
   }
 
   # Color loci by genotype
-  col <- c("gray", "black")[(muN == 1/2) + 1];
+  colMu <- c("gray", "black")[(muN == 1/2) + 1];
 
   for (tt in seq(along=tracks)) {
     track <- tracks[tt];
     verbose && enter(verbose, sprintf("Track #%d ('%s') of %d", 
                                              tt, track, length(tracks)));
 
-    pchT <- if (!is.null(scatter)) { pch } else { NA };
+    if (!is.null(scatter)) {
+      pchT <- pch;
+      colT <- col;
+    } else {
+      pchT <- NA;
+      colT <- NA;
+    }
 
     if (track == "tcn") {
-      plot(x, CT, pch=pchT, cex=cex, col="black", xlim=xlim, ylim=Clim, ylab="TCN");
-      stext(side=3, pos=1, chrTag);
-      if (grid) {
-        abline(h=seq(from=0, to=Clim[2], by=2), lty=3, col="gray");
-        abline(h=0, lty=1, col="black");
+      colT <- ifelse(is.null(colT), "black", colT);
+      if (add) {
+        points(x, CT, pch=pchT, col=colT, cex=cex);
+      } else {
+        plot(x, CT, pch=pchT, col=colT, cex=cex, xlim=xlim, ylim=Clim, ylab="TCN");
+        stext(side=3, pos=1, chrTag);
+        if (grid) {
+          abline(h=seq(from=0, to=Clim[2], by=2), lty=3, col="gray");
+          abline(h=0, lty=1, col="black");
+        }
+        drawConfidenceBands(fit, what="tcn", quantiles=quantiles, col="purple", xScale=xScale);
+        drawLevels(fit, what="tcn", col="purple", xScale=xScale);
       }
-      drawConfidenceBands(fit, what="tcn", quantiles=quantiles, col="purple", xScale=xScale);
-      drawLevels(fit, what="tcn", col="purple", xScale=xScale);
     }
   
     if (is.element(track, c("tcn,c1,c2", "tcn,c1", "tcn,c2", "c1,c2"))) {
+      colT <- ifelse(is.null(colT), "black", colT);
       subtracks <- strsplit(track, split=",", fixed=TRUE)[[1]];
       ylab <- paste(toupper(subtracks), collapse=", ");
-      plot(x, CT, pch=pchT, cex=cex, col="black", xlim=xlim, ylim=Clim, ylab=ylab);
-      stext(side=3, pos=1, chrTag);
-      if (grid) {
-        abline(h=seq(from=0, to=Clim[2], by=2), lty=3, col="gray");
-        abline(h=0, lty=1, col="black");
-      }
-      if (is.element("tcn", subtracks)) {
-        drawConfidenceBands(fit, what="tcn", quantiles=quantiles, col="purple", xScale=xScale);
-      }
-      if (is.element("c2", subtracks)) {
-        drawConfidenceBands(fit, what="c2", quantiles=quantiles, col="red", xScale=xScale);
-      }
-      if (is.element("c1", subtracks)) {
-        drawConfidenceBands(fit, what="c1", quantiles=quantiles, col="blue", xScale=xScale);
-      }
-      if (is.element("tcn", subtracks)) {
-        drawLevels(fit, what="tcn", col="purple", xScale=xScale);
-      }
-      if (is.element("c2", subtracks)) {
-        drawLevels(fit, what="c2", col="red", xScale=xScale);
-      }
-      if (is.element("c1", subtracks)) {
-        drawLevels(fit, what="c1", col="blue", xScale=xScale);
+      if (add) {
+        points(x, CT, pch=pchT, cex=cex, col=colT);
+      } else {
+        plot(x, CT, pch=pchT, cex=cex, col=colT, xlim=xlim, ylim=Clim, ylab=ylab);
+        stext(side=3, pos=1, chrTag);
+        if (grid) {
+          abline(h=seq(from=0, to=Clim[2], by=2), lty=3, col="gray");
+          abline(h=0, lty=1, col="black");
+        }
+        if (is.element("tcn", subtracks)) {
+          drawConfidenceBands(fit, what="tcn", quantiles=quantiles, col="purple", xScale=xScale);
+        }
+        if (is.element("c2", subtracks)) {
+          drawConfidenceBands(fit, what="c2", quantiles=quantiles, col="red", xScale=xScale);
+        }
+        if (is.element("c1", subtracks)) {
+          drawConfidenceBands(fit, what="c1", quantiles=quantiles, col="blue", xScale=xScale);
+        }
+        if (is.element("tcn", subtracks)) {
+          drawLevels(fit, what="tcn", col="purple", xScale=xScale);
+        }
+        if (is.element("c2", subtracks)) {
+          drawLevels(fit, what="c2", col="red", xScale=xScale);
+        }
+        if (is.element("c1", subtracks)) {
+          drawLevels(fit, what="c1", col="blue", xScale=xScale);
+        }
       }
     }
   
     if (track == "betaN") {
-      plot(x, betaN, pch=pchT, cex=cex, col=col, xlim=xlim, ylim=Blim, ylab=expression(BAF[N]));
-      stext(side=3, pos=1, chrTag);
+      colT <- ifelse(is.null(colT), colMu, colT);
+      if (add) {
+        points(x, CT, pch=pchT, cex=cex, col="black");
+      } else {
+        plot(x, betaN, pch=pchT, cex=cex, col=colT, xlim=xlim, ylim=Blim, ylab=expression(BAF[N]));
+        stext(side=3, pos=1, chrTag);
+      }
     }
   
     if (track == "betaT") {
-      plot(x, betaT, pch=pchT, cex=cex, col=col, xlim=xlim, ylim=Blim, ylab=expression(BAF[T]));
-      stext(side=3, pos=1, chrTag);
+      colT <- ifelse(is.null(colT), colMu, colT);
+      if (add) {
+        points(x, CT, pch=pchT, cex=cex, col="black");
+      } else {
+        plot(x, betaT, pch=pchT, cex=cex, col=colT, xlim=xlim, ylim=Blim, ylab=expression(BAF[T]));
+        stext(side=3, pos=1, chrTag);
+      }
     }
   
     if (track == "betaTN") {
-      plot(x, betaTN, pch=pchT, cex=cex, col=col, xlim=xlim, ylim=Blim, ylab=expression(BAF[T]^"*"));
-      stext(side=3, pos=1, chrTag);
+      colT <- ifelse(is.null(colT), colMu, colT);
+      if (add) {
+        points(x, CT, pch=pchT, cex=cex, col="black");
+      } else {
+        plot(x, betaTN, pch=pchT, cex=cex, col=colT, xlim=xlim, ylim=Blim, ylab=expression(BAF[T]^"*"));
+        stext(side=3, pos=1, chrTag);
+      }
     }
   
     if (track == "dh") {
@@ -231,10 +274,26 @@ setMethodS3("plotTracks", "PairedPSCBS", function(x, tracks=c("tcn", "dh", "tcn,
       naValue <- as.double(NA);
       rho <- rep(naValue, length=nbrOfLoci);
       rho[isHet] <- 2*abs(betaTN[isHet]-1/2);
-      plot(x, rho, pch=pchT, cex=cex, col=col[isHet], xlim=xlim, ylim=Blim, ylab="DH");
-      stext(side=3, pos=1, chrTag);
-      drawConfidenceBands(fit, what="dh", quantiles=quantiles, col="orange", xScale=xScale);
-      drawLevels(fit, what="dh", col="orange", xScale=xScale);
+      colT <- ifelse(is.null(colT), colMu[isHet], colT);
+      if (add) {
+        points(x, CT, pch=pchT, cex=cex, col="black");
+      } else {
+        plot(x, rho, pch=pchT, cex=cex, col=colT, xlim=xlim, ylim=Blim, ylab="DH");
+        stext(side=3, pos=1, chrTag);
+        drawConfidenceBands(fit, what="dh", quantiles=quantiles, col="orange", xScale=xScale);
+        drawLevels(fit, what="dh", col="orange", xScale=xScale);
+      }
+    }
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    # Draw change points?
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    if (changepoints) {
+      segs <- as.data.frame(fit);
+      xStarts <- segs[,"tcn.loc.start"];
+      xEnds <- segs[,"tcn.loc.end"];
+      xs <- sort(unique(c(xStarts, xEnds)));
+      abline(v=xScale*xs, lty=1, col="gray");
     }
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -840,6 +899,16 @@ setMethodS3("plotTracksManyChromosomes", "PairedPSCBS", function(x,   chromosome
 
 ############################################################################
 # HISTORY:
+# 2011-01-18
+# o DOCUMENTATION: Documented more plotTracks() arguments for PairedPSCBS.
+# o Now plotTracks(..., add=TRUE) for PairedPSCBS plots to the current
+#   figure/panel.
+# o Now plotTracks(..., add=FALSE) for PairedPSCBS only sets up subplots
+#   if argument 'tracks' specifies more than one panel.
+# o Added argument 'col' to plotTracks() for PairedPSCBS.
+# 2011-01-12
+# o Added argument 'changepoints' to plotTracks() for PairedPSCBS for
+#   highlightning change-point locations as vertical lines.
 # 2010-12-01
 # o Now using a default 'seed' for plotTracksManyChromosomes() such
 #   that the scatter data in the plots are reproducible by default.
