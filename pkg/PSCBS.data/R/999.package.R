b1d27611fb50eb835161eefdbf89a90192b3b5e1
henrikb <henrikb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-04-08 20:56:55 +0000

    o Created package 'PSCBS.data' by extracting example data from the 'PSCBS' package.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/abo@403 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/PSCBS.data/R/999.package.R
@@ -0,0 +1,30 @@
+#########################################################################/**
+# @RdocPackage "PSCBS.data"
+# @eval "rd <- gsub('\\alias{PSCBS.data}', '', rd, fixed=TRUE); ''"
+#
+# \description{
+#   @eval "packageDescription('PSCBS.data')$Description".
+# }
+#
+# \section{Installation and updates}{
+#   To install this package, use \code{install.packages("PSCBS.data")}.
+# }
+#
+# \section{How to cite}{
+#   Please use [1] and [2] to cite when using Paired PSCBS,
+#   and [3] and [4] when using CBS.
+# }
+#
+# @author
+#
+# \section{License}{
+#  @eval "packageDescription('PSCBS.data')$License".
+# }
+#
+# \references{
+#  [1] @include "../incl/OlshenA_etal_2011.Rd" \cr
+#  [2] @include "../incl/BengtssonH_etal_2010.Rd" \cr
+#  [3] @include "../incl/OlshenVenkatraman_2004.Rd" \cr
+#  [4] @include "../incl/VenkatramanOlshen_2007.Rd" \cr
+# }
+#*/#########################################################################
---
+++ pkg/PSCBS.data/R/paired.chr01.R
@@ -0,0 +1,29 @@
+#########################################################################/**
+# @RdocData "paired.chr01"
+#
+# @title "Matched tumor-normal SNP microarray data"
+#
+# \description{
+#  @get "title" on chromosome 1 (73,346 loci) for a single anonymous individual.
+# }
+#
+# \usage{paired.chr01}
+#
+# \format{
+#  A @data.frame with 73,346 rows (one per SNP) and columns
+#  'chromosome', 'x' 'CT', 'betaT', 'CN' and 'betaN'.
+# }
+#
+# \examples{
+#   library("PSCBS.data")
+#   data("paired.chr01")
+#   str(paired.chr01)
+# }
+#
+#
+# \references{
+#  [1] @include "../incl/OlshenA_etal_2011.Rd"
+# }
+#
+# @author
+#*/#########################################################################
